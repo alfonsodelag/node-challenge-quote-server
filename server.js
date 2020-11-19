3b37d21d03e26834fefce50e010cb7205edc692a
Alfonso <alfonsodelag1@gmail.com>
2020-11-19 21:27:07 +0100

    adding files to github

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 // server.js
 // This is where your node app starts
-
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
-
 //load the quotes JSON
 const quotes = require("./quotes.json");
+const quotesById = require("./quotes-with-id.json");
+const bodyparser = require('body-parser')
+const lodash = require('lodash');
+var cors = require('cors');
+const fs = require('fs');
 
+app.use(cors());
+app.use(bodyparser.json())
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
@@ -15,20 +20,82 @@ const quotes = require("./quotes.json");
 app.get("/", function (request, response) {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
-
 //START OF YOUR CODE...
+app.get("/quotes", (req, res) => {
+  res.json(quotes);
+});
+/* const getQuoteRandom = () => {
+  const randomNumber = Math.floor(Math.random()* quotes.length);
+  return quotes[randomNumber];
+}; */
+app.get("/quotes/random", (req, res) => {
+  const quote = lodash.sample(quotes);
+  res.json(quote);
+});
+//get quote by ID
+// You can use "id" as req.params because of the ":" before it.
+app.get("/quotes/:id", (req, res) => {
+  // ! En este caso creamos el "id" con "let" para poder modificarlo a Number
+  let id = req.params.id;
+
+  id = Number(id);
+  console.log(id);
+
+  const quote = quotesById.filter((el) => el.id === id);
+  console.log(quote)
+  res.send(quote);
+});
+
+// POST quote
+app.post("/quotes/addquote", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  console.log("req", req);
+  data.id = quotesById[quotesById.length - 1].id + 1;
+  let newQuoteJson = quotesById;
+  newQuoteJson.push(data);
+
+  fs.writeFile("./quotes-with-id.json", JSON.stringify(newQuoteJson), () => { })
+
+  res.send("POST your quote!!")
+})
 
 //...END OF YOUR CODE
-
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
 //example: pickFromArray(myContactsArray)
 //
+
+//DELETE quote
+app.delete("/quotes/:id", (req, res) => {
+  console.log("User is deleting a quote");
+  let id = req.params.id;
+  id = Number(id);
+  let newQuotesJson = quotesById;
+  let quote = newQuotesJson.filter((el) => el.id === id);
+  //[{}]
+  const index = newQuotesJson.indexOf(quote[0]);
+  newQuotesJson.splice(index, 1);
+  fs.writeFile("./quotes-with-id.json", JSON.stringify(newQuotesJson), () => { });
+  res.send("DELETE your quote!");
+});
+
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
-}
-
+};
+const searchQuotes = (term) => {
+  const newArr = [];
+  quotes.forEach(el => {
+    if (el.quote.includes(term)) newArr.push(el);
+  });
+  return newArr;
+};
+app.get("/quotes/search", (req, res) => {
+  const term = req.query.value;
+  const newQuotes = searchQuotes(term);
+  res.json(newQuotes);
+});
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(4000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
